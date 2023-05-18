994441ae131fe4e06c8ff76921027737069d0231
w3schools-test <aaronkaltman@gmail.com>
2023-05-18 12:52:15 -0400

    6.1.1

--- components/AaronHeader/AaronHeader.js
+++ components/AaronHeader/AaronHeader.js
@@ -5,12 +5,21 @@ import Typography from '@mui/material/Typography';
 import MenuItem from '@mui/material/MenuItem';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import { makeStyles } from '@mui/material';
+
+const useStyles = makeStyles({
+    toolbar: {
+        padding: 0,
+    },
+});
 
 function AaronHeader(props) {
+    const classes = useStyles();
+
     return (
         <AppBar position="fixed">
             <Container maxWidth="xl">
-                <Toolbar>
+                <Toolbar className={classes.toolbar}>
                     <Box display="flex" justifyContent="space-between" width="100%">
                         <Typography variant="h6">
                             Header Title
