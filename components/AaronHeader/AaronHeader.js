17dcefd5e6d646286aca89fd7eb353a26879a8ba
w3schools-test <aaronkaltman@gmail.com>
2023-05-18 10:10:08 -0400

    3

--- components/AaronHeader/AaronHeader.js
+++ components/AaronHeader/AaronHeader.js
@@ -3,20 +3,28 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import MenuItem from '@mui/material/MenuItem';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 function AaronHeader(props) {
     return (
         <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6">
-                    Header Title
-                </Typography>
-                {props.menu.map((item) => (
-                    <MenuItem key={item.id}>
-                        {item.label}
-                    </MenuItem>
-                ))}
-            </Toolbar>
+            <Container maxWidth="xl">
+                <Toolbar>
+                    <Box display="flex" justifyContent="space-between" width="100%">
+                        <Typography variant="h6">
+                            Header Title
+                        </Typography>
+                        <Box display="flex">
+                            {props.menu.map((item) => (
+                                <MenuItem key={item.id}>
+                                    {item.label}
+                                </MenuItem>
+                            ))}
+                        </Box>
+                    </Box>
+                </Toolbar>
+            </Container>
         </AppBar>
     );
 }
