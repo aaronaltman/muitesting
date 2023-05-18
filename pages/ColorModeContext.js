d0f94b8a0db3b809625d52b09deb8d5690b5c735
w3schools-test <aaronkaltman@gmail.com>
2023-05-18 15:10:26 -0400

    6.1.5 - dark mode

--- pages/ColorModeContext.js
+++ pages/ColorModeContext.js
@@ -1,3 +1,17 @@
 import React from 'react';
 
+// Create and export the context
 export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function ColorModeContextProvider({ children }) {
+    // You might want to add state here to manage the color mode
+
+    return (
+        <ColorModeContext.Provider value={{ toggleColorMode: () => {} }}>
+            {children}
+        </ColorModeContext.Provider>
+    );
+}
+
+export default ColorModeContextProvider;
+
