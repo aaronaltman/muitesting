5b5eccbad276aff62306e9bf675a67841162a84f
w3schools-test <aaronkaltman@gmail.com>
2023-05-18 15:54:39 -0400

    6.1.6 - dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,110 +1,20 @@
+import '../faust.config';
 import React from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { useRouter } from 'next/router';
+import { FaustProvider } from '@faustwp/core';
+import 'normalize.css/normalize.css';
+import '../styles/main.scss';
+import ThemeStyles from 'components/ThemeStyles/ThemeStyles';
 
-import AaronHeader from '../components/AaronHeader/AaronHeader.js';
-
-import ColorModeContextProvider from './ColorModeContext';
-import {FaustProvider} from "@faustwp/core";
-
-
-function App() {
-    const [mode] = React.useState('light');
-
-    const theme = React.useMemo(
-        () =>
-            createTheme({
-                palette: {
-                    mode,
-                },
-                typography: {
-                    fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
-                    fontSize: 14,
-                    h1: {
-                        fontWeight: 700,
-                        fontSize:'1.8rem',
-                    },
-                    h2: {
-                        fontWeight: 700,
-                    },
-                    h3: {
-                        fontWeight: 600,
-                    },
-                    h4: {
-                        fontWeight: 600,
-                    },
-                    h5: {
-                        fontWeight: 500,
-                    },
-                    h6: {
-                        fontWeight: 500,
-                        fontSize:'2.5rem',
-                    },
-                    subtitle1: {
-                        fontSize: 16,
-                    },
-                    subtitle2: {
-                        fontSize: 14,
-                    },
-                    body1: {
-                        fontSize: 16,
-                    },
-                    body2: {
-                        fontSize: 14,
-                    },
-                    button: {
-                        textTransform: 'none',
-                    },
-                    caption: {
-                        fontSize: 12,
-                    },
-                    overline: {
-                        fontSize: 10,
-                    },
-                },
-                components: {
-                    MuiMenuItem: {
-                        styleOverrides: {
-                            root: {
-                                color: '#333',
-                                '&:hover': {
-                                    backgroundColor: '#f5f5f5',
-                                },
-                                '&.Mui-selected': {
-                                    backgroundColor: '#ddd',
-                                },
-                                '&.Mui-selected:hover': {
-                                    backgroundColor: '#ccc',
-                                },
-                            },
-                        },
-                    },
-                    MuiToolbar: {
-                        styleOverrides: {
-                            root: {
-                                padding: 0,
-                            },
-                        },
-                    },
-                },
-            }),
-        [mode],
-    );
-
-    const menuItems = [
-        { id: 1, label: 'Home' },
-        { id: 2, label: 'About' },
-        // other menu items...
-    ];
+export default function MyApp({ Component, pageProps }) {
+    const router = useRouter();
 
     return (
-        <ColorModeContextProvider>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <AaronHeader menu={menuItems} />
-            </ThemeProvider>
-        </ColorModeContextProvider>
+        <>
+            <ThemeStyles />
+            <FaustProvider pageProps={pageProps}>
+                <Component {...pageProps} key={router.asPath} />
+            </FaustProvider>
+        </>
     );
 }
-
-export default App;
