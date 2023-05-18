ccb0f66d076ce314275d3c2f500bf705566bf747
w3schools-test <aaronkaltman@gmail.com>
2023-05-18 10:23:48 -0400

    3

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,34 @@ import { FaustProvider } from '@faustwp/core';
 import 'normalize.css/normalize.css';
 import '../styles/main.scss';
 import ThemeStyles from 'components/ThemeStyles/ThemeStyles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// Create a theme instance.
+const theme = createTheme({
+    typography: {
+        fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+        fontSize: 14,
+        h1: {
+            fontWeight: 700,
+        },
+        h2: {
+            fontWeight: 700,
+        },
+        // ...and so on for each typography variant
+    },
+});
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <>
-      <ThemeStyles />
-      <FaustProvider pageProps={pageProps}>
-        <Component {...pageProps} key={router.asPath} />
-      </FaustProvider>
-    </>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ThemeStyles />
+            <FaustProvider pageProps={pageProps}>
+                <Component {...pageProps} key={router.asPath} />
+            </FaustProvider>
+        </ThemeProvider>
+    );
 }
