f0a81ed422250da5122c4abd73e2f926cc65ec98
w3schools-test <aaronkaltman@gmail.com>
2023-05-18 20:52:33 -0400

    6.1.9 - front page edits

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -4,7 +4,6 @@ import { useQuery, gql } from '@apollo/client';
 import { FaArrowRight } from 'react-icons/fa';
 import styles from 'styles/pages/_Home.module.scss';
 import {
-  EntryHeader,
   Main,
   Button,
   Heading,
@@ -35,11 +34,6 @@ export default function Component() {
   const primaryMenu = data?.headerMenuItems?.nodes ?? [];
   const footerMenu = data?.footerMenuItems?.nodes ?? [];
 
-  const mainBanner = {
-    sourceUrl: '/static/banner.jpeg',
-    mediaDetails: { width: 1200, height: 600 },
-    altText: 'Portfolio Banner',
-  };
   return (
     <>
       <SEO title={siteTitle} description={siteDescription} />
@@ -48,22 +42,7 @@ export default function Component() {
       <AaronHeroNew />
 
       <Main className={styles.home}>
-        <EntryHeader image={mainBanner} />
         <div className="container">
-          <section className="cta">
-            <CTA
-              Button={() => (
-                <Button href="/posts">
-                  Get Started <FaArrowRight style={{ marginLeft: `1rem` }} />
-                </Button>
-              )}
-            >
-              <span>
-                Learn about Core Web Vitals and how Atlas can help you reach
-                your most demanding speed and user experience requirements.
-              </span>
-            </CTA>
-          </section>
           <section className={styles.posts}>
             <Heading className={styles.heading} level="h2">
               Latest Posts
