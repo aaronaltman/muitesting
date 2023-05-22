6d42e280f72bad4faf4d5e2549a6ac2381f79b7d
w3schools-test <aaronkaltman@gmail.com>
2023-05-22 14:55:51 -0400

    6.3.5 - integrating GQL  blog

--- components/AaronBlog/AaronBlog.js
+++ components/AaronBlog/AaronBlog.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import BlogReachView from '/src/views/BlogReachView';
+import Page from "/src/components/Page";
 
 
 function AaronBlog (){
    return(
+       <Page>
        <BlogReachView/>
+       </Page>
    )
 }
 
