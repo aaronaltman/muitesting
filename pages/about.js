091cf8b01a572a0bb0b5572c80786d2a14c5cee4
w3schools-test <aaronkaltman@gmail.com>
2023-05-22 08:33:02 -0400

    6.3.0 - I got dark mode going! now let's see if we can add a page! - about page - try again

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import Page from '/src/components/Page';
+import About from '/src/views/About';
 
-import {About} from "../src/views";
 import Topbar from "../src/layouts/Fixed/components/Topbar";
 
 function aboutPage (){
