48204e08e95f54a315094cc361d4e9bf9975feac
w3schools-test <aaronkaltman@gmail.com>
2023-05-22 08:48:58 -0400

    6.3.1 - the about page works - have to take off topbar - pretty fucking COOOOOOL

--- pages/about.js
+++ pages/about.js
@@ -3,12 +3,9 @@ import React from 'react';
 import Page from '/src/components/Page';
 import About from '/src/views/About';
 
-import Topbar from "../src/layouts/Fixed/components/Topbar";
-
 function aboutPage (){
     return(
         <Page>
-            <Topbar />
         <About />
         </Page>
     )
