4cebd52494bc7e79f962b73ca134863bb0afc18b
w3schools-test <aaronkaltman@gmail.com>
2023-05-22 22:37:07 -0400

    6.3.8 headless WP change lets see what happens

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -1,82 +1,74 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { alpha, useTheme } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
+import Typography from "@mui/material/Typography";
 
-const Topbar = ({ onSidebarOpen, colorInvert = false }) => {
-  const theme = useTheme();
-  const { mode } = theme.palette;
-  return (
-      <Box
-          display={'flex'}
-          justifyContent={'space-between'}
-          alignItems={'center'}
-          width={1}
-      >
+const Topbar = ({ onSidebarOpen, }) => {
+    const theme = useTheme();
+    //const { mode } = theme.palette;
+    return (
         <Box
             display={'flex'}
-            component="a"
-            href="/"
-            title="theFront"
-            width={{ xs: 100, md: 120 }}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            width={1}
         >
-          <Box
-              component={'img'}
-              src={
-                mode === 'light' && !colorInvert
-                    ? 'https://assets.maccarianagency.com/the-front/logos/logo.svg'
-                    : 'https://assets.maccarianagency.com/the-front/logos/logo-negative.svg'
-              }
-              height={1}
-              width={1}
-          />
-        </Box>
-        <Box
-            display={'flex'}
-            width={{ xs: 100, md: 120 }}
-        >
-        </Box>
-        <Box sx={{ display: { xs: 'none', md: 'flex' } }} alignItems={'center'}>
-          <Box marginLeft={4}>
-            <Button
-                variant="contained"
-                color="primary"
+            <Box
+                display={'flex'}
                 component="a"
-                target="blank"
-                href="https://mui.com/store/items/the-front-landing-page/"
-                size="large"
+                href="/"
+                title="theFront"
+                width={{ xs: 100, md: 120 }}
             >
-              Buy now
-            </Button>
-          </Box>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1 }}
+                >
+                    Headless WP
+                </Typography>
+            </Box>
+            <Box
+                display={'flex'}
+                alignItems={'center'}
+                sx={{ display: { xs: 'none', md: 'flex' } }}
+            >
+                <Box marginLeft={4}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        component="a"
+                        target="blank"
+                        href="https://mui.com/store/items/the-front-landing-page/"
+                        size="large"
+                    >
+                        Buy now
+                    </Button>
+                </Box>
+            </Box>
+            <Box
+                display={'flex'}
+                alignItems={'center'}
+                sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+                <Button
+                    onClick={() => onSidebarOpen()}
+                    aria-label="Menu"
+                    variant={'outlined'}
+                    sx={{
+                        borderRadius: 2,
+                        minWidth: 'auto',
+                        padding: 1,
+                        borderColor: alpha(theme.palette.divider, 0.2),
+                    }}
+                >
+                    <MenuIcon />
+                </Button>
+            </Box>
         </Box>
-        <Box sx={{ display: { xs: 'block', md: 'none' } }} alignItems={'center'}>
-          <Button
-              onClick={() => onSidebarOpen()}
-              aria-label="Menu"
-              variant={'outlined'}
-              sx={{
-                borderRadius: 2,
-                minWidth: 'auto',
-                padding: 1,
-                borderColor: alpha(theme.palette.divider, 0.2),
-              }}
-          >
-            <MenuIcon />
-          </Button>
-        </Box>
-      </Box>
-  );
-};
-
-Topbar.propTypes = {
-  onSidebarOpen: PropTypes.func,
-  cx: PropTypes.func,
-  navClasses: PropTypes.string,
-  menuItems: PropTypes.array,
-  colorInvert: PropTypes.bool,
+    );
 };
 
 export default Topbar;
